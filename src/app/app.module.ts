618ae09fce684be419a6a769ae344da12dee9a81
jersoncarranza <jerson_carranza2@hotmail.com>
2023-08-08 13:57:26 -0500

    git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment.development';
 
+const config: SocketIoConfig = { 
+  url: environment.wsUrl , options: {} 
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
